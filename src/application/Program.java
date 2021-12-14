40908859540554ae3328f72a04a8703c7558d404
LucasRPontes <lucasrpontesgarcia@gmail.com>
2021-12-14 03:35:00 -0300

    create seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model_entities.Department;
+import model_entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(3, "lucas");
-		System.out.print(obj);
+		Department obj = new Department(3, "dev");
+		Seller seller = new Seller(21, "lucas", "a@a.com", new Date(), 3500.0, obj);
+		System.out.print(seller);
 	}
 
 }
